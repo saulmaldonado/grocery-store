f0b515e9f9a6c0270619f7108d4bb636d86f8d99
Saul Maldonado <samaldonado4@gmail.com>
2020-06-02 01:11:14 -0500

    generated components, added routes to navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,52 @@ import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
+import { MyordersComponent } from './myorders/myorders.component';
+import { LoginComponent } from './login/login.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { ProductsComponent } from './products/products.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ShoppingcartComponent,
+    MyordersComponent,
+    LoginComponent,
+    AdminOrdersComponent,
+    AdminProductsComponent,
+    ProductsComponent,
+    NotFoundComponent,
+  ],
   imports: [
     BrowserModule,
     AngularFirestoreModule,
+    AngularFireAuthModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
+    NgbModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'orders', component: MyordersComponent },
+      { path: 'shoppingcart', component: ShoppingcartComponent },
+      { path: 'login', component: LoginComponent },
+      {
+        path: 'admin',
+        children: [
+          { path: 'orders', component: AdminOrdersComponent },
+          { path: 'products', component: AdminProductsComponent },
+        ],
+      },
+      { path: '**', component: NotFoundComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent],
